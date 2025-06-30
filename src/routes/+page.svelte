bf3892e48140942cbc8b7689070befd28ce8ed0b
MrZeus46 <117559099+MrZeus46@users.noreply.github.com>
2025-06-30 08:32:41 +0530

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="flex justify-center items-center h-screen bg-gradient-to-tr from-red-300 to-blue-400">
+    <h1 class="text-white text-3xl font-semibold italic uppercase">
+        Hello world!
+    </h1>
+</div>
