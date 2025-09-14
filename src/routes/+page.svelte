12716ac9eeb6b072749e17c09854f16dc4d7f91f
Akhil <akhil.a@gnxsolutions.in>
2025-09-15 00:26:58 +0530

    homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,43 @@
-<div class="flex justify-center items-center h-screen bg-gradient-to-tr from-red-300 to-blue-400">
-    <h1 class="text-white text-3xl font-semibold italic uppercase">
-        Hello world!
-    </h1>
+<div class="flex md:flex-row flex-col px-4 items-center h-screen bg-gray-950">
+  <div
+    class="h-5/6 w-1/4 flex flex-col items-center justify-center hover:transform hover:scale-105 hover:rotate-x-2 duration-400 bg-transparent border border-white rounded-xl shadow-2xl shadow-amber-100"
+  >
+    <div
+      class="h-60 w-50 bg-white rounded-md contain-content flex flex-col items-center justify-center"
+    >
+      <img src="./assets/images/me.jpg" alt="Akhil" />
+    </div>
+    <div class="flex flex-col items-center gap-1 mt-4">
+      <p class="text-white text-lg font-semibold mt-4">imakhil46@gmail.com</p>
+      <p class="text-gray-600 text-sm font-semibold">Based in India</p>
+      <div class="flex gap-2 mt-4">
+        <a href="https://github.com/imakhil46">
+          <img
+            src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"
+            alt="GitHub"
+          />
+        </a>
+        <a href="https://twitter.com/akhil_46">
+          <img
+            src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white"            alt="Twitter"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/akhil-46/">
+          <img
+            src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"
+            alt="LinkedIn"
+          />
+        </a>
+        <a href="https://www.instagram.com/akhil_46/">
+          <img
+            src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white"
+            alt="Instagram"
+          />
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="h-5/6 w-2/3 flex flex-col items-center justify-center">
+    <h1 class="text-white text-3xl font-semibold">Hello world!</h1>
+  </div>
 </div>
